aeab6fbd7cc3d88634063147ecb352c60028a280
Steve Smythe <steve@smythefamily.com>
2019-01-07 15:28:15 -0800

    Update prod_deploy.sh

--- prod_deploy.sh
+++ prod_deploy.sh
@@ -1,4 +1,4 @@
-CID=$(docker ps -f NAME=hello-apache-prod -q)
+CID=$(docker ps -a -q -f NAME=hello-apache-prod)
 test -z "$CID" || (docker kill $CID; docker rm hello-apache-prod)
 docker run -dit --name hello-apache-prod -p 8380:80 hello-apache
 curl -sSL localhost:8380 | grep -q -i 'Hello, Apache!'
