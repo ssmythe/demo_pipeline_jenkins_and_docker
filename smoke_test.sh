c0b7c132f05c1b65cbe2a6b0b2f0041744fa9e6d
Steve Smythe <steve@smythefamily.com>
2019-01-07 15:29:49 -0800

    Update smoke_test.sh

--- smoke_test.sh
+++ smoke_test.sh
@@ -1,4 +1,4 @@
-CID=$(docker ps -a -f NAME=hello-apache-smoke-test)
+CID=$(docker ps -a -q -f NAME=hello-apache-smoke-test)
 test -z "$CID" || (docker kill $CID; docker rm hello-apache-smoke-test)
 docker run -dit --name hello-apache-smoke-test -p 8180:80 hello-apache
 curl -sSL localhost:8180 | grep -q -i 'Hello, Apache!'
