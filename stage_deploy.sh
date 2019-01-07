f1f60a44be898d62d97bd7d231716a9deb4b591d
Steve Smythe <steve@smythefamily.com>
2019-01-07 15:28:34 -0800

    Update stage_deploy.sh

--- stage_deploy.sh
+++ stage_deploy.sh
@@ -1,4 +1,4 @@
-CID=$(docker ps -f NAME=hello-apache-stage -q)
+CID=$(docker ps -a -q -f NAME=hello-apache-stage)
 test -z "$CID" || (docker kill $CID; docker rm hello-apache-stage)
 docker run -dit --name hello-apache-stage -p 8280:80 hello-apache
 curl -sSL localhost:8280 | grep -q -i 'Hello, Apache!'
